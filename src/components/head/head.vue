9db85bdf67039ce66a8cd9b42da520abd1859593
DreamyTZK <dreamytzk@outlook.com>
2021-07-03 18:16:59 +0800

    init

---
+++ src/components/head/head.vue
@@ -0,0 +1,16 @@
+<!-- 头部组件 -->
+<template>
+  <div class="test">
+    page
+  </div>
+</template>
+
+<style>
+.test{
+  color: red;
+}
+</style>
+
+<script>
+
+</script>
---
+++ src/components/main.js
@@ -0,0 +1,15 @@
+// 导入头部组件
+import head from '../components/head/head.vue'
+
+/**
+ * 全局filter
+ */
+import filterInit from '../util/filter'
+
+
+filterInit(Vue);
+
+Vue.component('xk-head', head)
+
+Vue.config.devtools = true
+window.Vue = Vue;
