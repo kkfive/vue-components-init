f857d0f387bb31d4e99199685b9c097d10e44d10
DreamyTZK <dreamytzk@outlook.com>
2021-12-12 11:50:26 +0800

    fix: 修改bug

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
-<script setup>
+<script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import { ref } from 'vue'
 import XkHead from './components/head/head.vue'
 let msg = ref('测试')
 setTimeout(() => {
-  msg += '123'
+  msg.value += '123'
 }, 1000)
 </script>
 
